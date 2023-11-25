0085d6863e5da57edcc4a3158307c619de389b2c
NumiKun <nugrohorizki20@gmail.com>
2023-11-25 19:40:18 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+var jsonParser = bodyParser.json()
+
+var urlencodeParser = bodyParser.urlencoded({ extended: false })
+const port = 3000
+
+app.use(cors())
+app.use(jsonParser);
+app.use(urlencodeParser);
+
+app.get('/', (req, res) => {
+  res.json('Hello World!')
+});
+
+app.post('/login', (req, res) => {
+    const username = 'newjeans'
+    const password = '12345678'
+
+    // check if username is valid
+    if (req.body.username !== username) {
+        res.json({ status: 'error, username not found' })
+    }
+
+    // check if password is valid
+    if (req.body.password !== password) {
+        res.json({ status: 'error, wrong password' })
+    }
+
+    res.json({ status: 'success' })
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
